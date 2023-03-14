71917cf83495e1a13a76e35ca97059bf2ae48fe5
Luis A <almonteluis92@gmail.com>
2023-03-14 12:29:03 -0400

    add: data.json to public to be copied during build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        {
+          src: "public/data.json",
+          dest: "dist",
+        },
+      ],
+    }),
+  ],
   base: "/frontend-mentor/",
 });
